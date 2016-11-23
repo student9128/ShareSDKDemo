4ccbdd7ba0d919b9af66561c9eba7b5fd6069c12
student9128 <student9128@gmail.com>
2016-11-23 11:08:28 +0800

    customize share content

--- app/src/main/java/com/kevin/tech/sharesdkdemo/MainActivity.java
+++ app/src/main/java/com/kevin/tech/sharesdkdemo/MainActivity.java
@@ -41,21 +41,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         oks.disableSSOWhenAuthorize();
 
 // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
-        oks.setTitle("标题");
+//        oks.setTitle("标题");
+        oks.setTitle("Kevin Blog");
 // titleUrl是标题的网络链接，QQ和QQ空间等使用
-        oks.setTitleUrl("http://sharesdk.cn");
+//        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl("http://blog.csdn.net/student9128");
 // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+//        oks.setText("我是分享文本");
+        oks.setText("Blog");
 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
 //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+oks.setImagePath("http://avatar.csdn.net/8/1/5/1_student9128.jpg");//确保SDcard下面存在此张图片
 // url仅在微信（包括好友和朋友圈）中使用
         oks.setUrl("http://sharesdk.cn");
 // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
+//        oks.setComment("我是测试评论文本");
+        oks.setComment("The Test Text");
 // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
+        oks.setSite(getString(R.string.tips));
 // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
+//        oks.setSiteUrl("http://sharesdk.cn");
+        oks.setSiteUrl("http://blog.csdn.net/student9128");
         oks.setSilent(false);// 显示编辑页面
 // 启动分享GUI
         oks.show(this);
